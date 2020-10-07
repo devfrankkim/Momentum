3f10faa204830a9e81cab8fa008f94f878d3aefd
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-10-07 13:08:16 -0400

    feat: added greeting features

--- js/greeting.js
+++ js/greeting.js
@@ -1,10 +1,14 @@
+const intro = document.querySelector(".intro");
+const introContainer = document.querySelector(".intro-container");
 const nameForm = document.querySelector(".input-name");
-const checkName = document.querySelector(".check-name");
+const checkName = document.getElementById("intro__input");
+const introContinue = document.getElementById("intro__continue");
 let greeting = document.querySelector(".greetings");
 let getLSG = localStorage.getItem("name");
 
 if (getLSG) {
   checkMorningNight(getLSG);
+  introContainer.classList.add("showCSS");
 }
 
 function checkMorningNight(value) {
@@ -21,10 +25,33 @@ function checkMorningNight(value) {
   greeting.style.textTransform = "capitalize";
 }
 
+function placeholderColor(value) {
+  value.style.width = "15rem";
+  value.placeholder = "Please write your beautiful name.";
+  value.classList.add("placeholderred");
+}
+
 nameForm.addEventListener("submit", (e) => {
   e.preventDefault();
   let checkValue = checkName.value;
-  checkName.style.display = "none";
-  localStorage.setItem("name", checkValue);
-  checkMorningNight(checkValue);
+  if (checkValue) {
+    checkName.style.display = "none";
+    localStorage.setItem("name", checkValue);
+    checkMorningNight(checkValue);
+    introContainer.classList.add("showCSS");
+  } else {
+    placeholderColor(checkName);
+  }
+});
+
+introContinue.addEventListener("click", (e) => {
+  e.preventDefault();
+  let targetName = e.target.previousElementSibling.children[0].value;
+  if (targetName) {
+    localStorage.setItem("name", targetName);
+    checkMorningNight(targetName);
+    introContainer.classList.add("showCSS");
+  } else {
+    placeholderColor(checkName);
+  }
 });
