7099e0d7db3293699a2ea1888536739a8068e6b9
heyfranksmile@gmail.com <heyfranksmile@gmail.com>
2020-11-02 16:20:25 -0500

    feat: added background img

---
+++ js/bg.js
@@ -0,0 +1,62 @@
+const images = ['../img/favicon.png','../img/physics.ico']
+let URLs = "picsum.photos/v2/list?page=2&limit=50"
+
+
+
+
+async function fetchData(limitImgs){
+
+    url = `https://picsum.photos/v2/list?page=2&limit=${limitImgs}`
+
+    try{
+        const response = await fetch(url);
+        const commits = await response.json(); // read response body and parse as JSON
+
+        renderPage(commits)
+        return commits
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
+
+// ======= Get URLs limit datas =======
+function getLimitNumber(){
+    const urlArray = URLs.split("/");
+    const limit = urlArray[urlArray.length - 1];
+    const limitNumber = limit.slice(limit.lastIndexOf('=') + 1);
+    return limitNumber
+}
+
+function template(){
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpeg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+
+function renderPage(datas){
+    let random = Math.floor(Math.random() * datas.length)
+    let {download_url : URLimg} = datas[random]
+
+
+    let image = new Image();
+    image.src = URLimg;
+    image.classList.add("bgImage");
+    document.body.prepend(image);
+
+
+    // document.body.style.backgroundImage = `url(${URLimg})`;
+    // document.body.classList.add('bgImage')
+}
+
+
+function init(){
+    const limitImgs = getLimitNumber()
+    fetchData(limitImgs)
+    // renderPage(datas);
+    
+}
+
+init();
